f10bfc9913463fcc6ad2678c7a0e8cda96253fb9
jaf7 <janthonyfields@gmail.com>
2017-03-19 15:10:55 -0600

    Add deep-equal.js

---
+++ Chapter_4_Objects _Arrays/deep-equal.js
@@ -0,0 +1,40 @@
+// Given two values, return true only if values are the same. Or
+// if objects, they have the same properties whose values are also equal
+// Compare objects using a recursive call
+
+function deepEqual( a, b ) {
+	// base case (end of nested objects), testing value identity
+	if ( a === b ) return true;
+
+	if ( typeof a != "object" || typeof b != "object" ) { return false; }
+	//typeof null returns Object, avoid returning true
+	if ( a === null || b === null ) { return false; }
+
+	//use counters to compare number of properties in a & b
+	var propertyCountA = 0, propertyCountB = 0;
+	for ( var aProperties in a ) {
+		propertyCountA ++;
+	}
+	for ( var bProperties in b ) {
+		propertyCountB ++;
+	}
+	if ( propertyCountA != propertyCountB ) { return false; }
+
+	// check for same properties & value equivalence (recurse & descend to base values)
+	for ( var property in a ) {
+		if ( !(property in b) ) { return false; }
+		// recursive case, descend & compare
+		if ( !( deepEqual(a[property], b[property]) ) ) { return false; }
+	}
+	return true;
+}
+// could have been more concise, only looping over a & b once each, study book solution
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+console.log(deepEqual(1,1));
