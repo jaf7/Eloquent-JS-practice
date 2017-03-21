e19820ba45b187a0748cf88ea236c0870a0f4802
jaf7 <janthonyfields@gmail.com>
2017-03-21 17:43:27 -0600

    Added age-difference.js

---
+++ Chapter_5_Higher_Order_Functions/flatten-array.js
@@ -0,0 +1,10 @@
+var arrays = [[1, 2, 3], [4, 5], [6]];
+
+var flatten = function( previous, current ) { return previous.concat( current ); }
+arrays.reduce( flatten );
+
+// arrays.reduce( function( pre, cur ) {
+// 	return pre.concat( cur );
+// });
+
+// → [1, 2, 3, 4, 5, 6]
