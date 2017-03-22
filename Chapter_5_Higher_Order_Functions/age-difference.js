d018424b1b03184a375afc5f341722cba6158ec1
jaf7 <janthonyfields@gmail.com>
2017-03-22 13:04:13 -0600

    Edit comments in age-difference.js

--- Chapter_5_Higher_Order_Functions/age-difference.js
+++ Chapter_5_Higher_Order_Functions/age-difference.js
@@ -1,4 +1,5 @@
 // Code follows JSON data
+// (is there a way to import this from other file outside of Node or html with <script> ?)
 var ANCESTRY_FILE = "[\n  " + [
   '{"name": "Carolus Haverbeke", "sex": "m", "born": 1832, "died": 1905, "father": "Carel Haverbeke", "mother": "Maria van Brussel"}',
   '{"name": "Emma de Milliano", "sex": "f", "born": 1876, "died": 1956, "father": "Petrus de Milliano", "mother": "Sophia van Damme"}',
