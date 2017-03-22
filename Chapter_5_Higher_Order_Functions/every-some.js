81c61320372dddf3edc65df5546a49acaaf042c1
jaf7 <janthonyfields@gmail.com>
2017-03-22 14:55:11 -0600

    Add every-some.js

---
+++ Chapter_5_Higher_Order_Functions/every-some.js
@@ -0,0 +1,25 @@
+// implement (define) array functions every and some
+// which operate like the methods of the same names
+
+function every( array, f ) {
+	for ( var i = 0; i < array.length; i++ ) {
+		if ( !f(array[i]) ) { return false; }
+	}
+	return true;
+}
+
+function some( array, f ) {
+	for ( var i = 0; i < array.length; i++ ) {
+		if ( f(array[i]) ) { return true; }
+	}
+	return false;
+}
+
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
